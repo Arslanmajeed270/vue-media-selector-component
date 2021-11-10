877670f18fa78a3aaf653fb4c16690e827272d95
Majeed <Arslan.Majeed@afiniti.com>
2021-11-10 13:04:57 +0500

    media uploader pop grid design complted

---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<style scoped>
+
+.addmargin {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+.vue-logo-back {
+    background-color: black;
+}
+
+</style>
+
+<template>
+
+<div class="home">
+    <div class="vue-logo-back">
+        <img src="../assets/logo.png" width="100px" height="100px">
+    </div>  
+</div>
+
+</template>
+
+<script>
+
+// @ is an alias to /src
+import axios from 'axios'
+
+export default {
+    name: 'home',
+    mounted() {
+        axios({
+            method: "GET",
+            "url": "assets/samplejson/customerlist.json"
+        }).then(response => {
+            this.customerlist = response.data;
+        }, error => {
+            // eslint-disable-next-line
+            console.error(error);
+        });
+    },
+    data() {
+        return {
+            customerlist: [],
+            selectedCustomer: ""
+        }
+    },
+    methods: {
+        setSelectedCustomer: function(name) {
+            this.selectedCustomer = name;
+        },
+        goToDetailsPage: function(id) {
+            this.$router.push("/customerdetails/"+id);
+        }
+    }
+}
+
+</script>
